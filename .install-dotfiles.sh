e2f77d39a449443a48079c39fc7fe7847abd7326
Ian Costa <10099038+Ian-Costa18@users.noreply.github.com>
2024-08-31 18:34:36 -0400

    Whoops, now changes are added

--- .install-dotfiles.sh
+++ .install-dotfiles.sh
@@ -5,3 +5,5 @@ alias config='/usr/bin/git --git-dir=$HOME/.myconfig/ --work-tree=$HOME'
 config config --local status.showUntrackedFiles no
 config checkout
 echo "If there were errors, delete those files then rerun: config checkout"
+sudo pacman -Syu alacritty fish neovim qtile rofi exa unimatrix ranger
+echo "powerline-shell and vim-plug needs to be installed using the AUR"
